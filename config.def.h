d40bd6ea4a30d9abfa65f9092109df9d3f9f1afe
dk949 <56653556+dk949@users.noreply.github.com>
2022-07-03 14:31:28 +0300

    applied ligatures

--- config.def.h
+++ config.def.h
@@ -5,8 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=32:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:pixelsize=32:antialias=true:autohint=true";
 static int borderpx = 2;
+
 
 /*
  * What program is execed by st depends of these precedence rules:
