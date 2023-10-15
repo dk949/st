cbea961c4765ab106b90106662e05ee0c22feb64
dk949 <56653556+dk949@users.noreply.github.com>
2023-10-15 18:56:31 +0100

    make it possible to set version from an env variable

--- config.mk
+++ config.mk
@@ -1,8 +1,10 @@
 # st version
+ifndef VERSION
 DATE         = $(shell git log -1 --format='%cd' --date=format:'%F')
 DATE_TIME    = $(DATE) 00:00
 COMMIT_COUNT = $(shell git rev-list --count HEAD --since="$(DATE_TIME)")
 VERSION      = 0.8.5.$(shell date -d "$(DATE)" +'%Y%m%d')_$(COMMIT_COUNT)
+endif
 
 # Customize below to fit your system
 
