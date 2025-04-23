73f2de2c6cd772a4c58180f4f583787346a8c935
dk949 <56653556+dk949@users.noreply.github.com>
2025-04-23 16:12:39 +0100

    makefile fixes

--- config.mk
+++ config.mk
@@ -22,10 +22,8 @@ LIBS     = `pkg-config $(REQ_LIBS) --libs` -lm -lrt -lutil
 
 # flags
 CPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600
-CFLAGS  ?=
-LDFLAGS ?=
-CFLAGS  += $(LIBFLAGS) $(CPPFLAGS) -O3
-LDFLAGS += $(LIBS)
+CFLAGS  = $(LIBFLAGS) $(CPPFLAGS) -O3
+LDFLAGS = $(LIBS)
 
 # compiler and linker
 CC ?= gcc
